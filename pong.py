0a4b5b709f96361797cba9ff9714f4357a95a5df
Josh Somma <jsomma@squiz.com>
2015-08-19 09:30:19 +0100

    latest updates

--- pong.py
+++ pong.py
@@ -13,8 +13,10 @@ HALF_PAD_WIDTH = PAD_WIDTH / 2
 HALF_PAD_HEIGHT = PAD_HEIGHT / 2
 LEFT = False
 RIGHT = True
-paddle1_pos = [0, HEIGHT / 2 - HALF_PAD_HEIGHT]
-paddle2_pos = [WIDTH, HEIGHT / 2 - HALF_PAD_HEIGHT]
+paddle1_pos = HEIGHT / 2
+paddle2_pos = HEIGHT / 2
+paddle1_vel = 0
+paddle2_vel = 0
 
 
 # initialize ball_pos and ball_vel for new ball in middle of table
@@ -27,7 +29,8 @@ def spawn_ball(direction):
     if direction == RIGHT:
         ball_vel = [random.randrange(120, 240) / 60,random.randrange(60, 180) / 60]
     elif direction == LEFT:
-        ball_vel = [random.randrange(120, 240) / 60,-random.randrange(60, 180) / 60]
+        ball_vel = [random.randrange(120, 240) / 180,random.randrange(60, 180) / 180]
+
 # define event handlers
 def new_game():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
@@ -36,13 +39,13 @@ def new_game():
 
 def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
- 
-        
+    
+    #print coords for testing
+
     # draw mid line and gutters
     canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
     canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
     canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
-    canvas.draw_line([0, HEIGHT / 2],[WIDTH, HEIGHT / 2], 1, "White")
 
     # update ball
     ball_pos[0] += ball_vel[0]
@@ -52,18 +55,14 @@ def draw(canvas):
     #check vertical position and reverse
     if ball_pos[1] <= BALL_RADIUS:
        ball_vel[1] = - ball_vel[1]
-       print "hit1"
     if ball_pos[1] >= HEIGHT - BALL_RADIUS:
        ball_vel[1] = - ball_vel[1]
-       print "hit2"
 
     #check horizontal position and reverse
-    if ball_pos[0] <= PAD_WIDTH + BALL_RADIUS:
-       ball_vel[0] = - ball_vel[0]
-       print "hit1"
+    #if ball_pos[0] <= PAD_WIDTH + BALL_RADIUS:
+       #ball_vel[0] = - ball_vel[0]
     if ball_pos[0] >= WIDTH - (PAD_WIDTH + BALL_RADIUS):
        ball_vel[0] = - ball_vel[0]
-       print "hit2"
     
 
             
@@ -71,25 +70,60 @@ def draw(canvas):
     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
     
     # update paddle's vertical position, keep paddle on the screen
-    def move_up(key):
+    if (paddle1_pos - HALF_PAD_HEIGHT) + paddle1_vel > 0:
+        paddle1_pos += paddle1_vel
+    if (paddle1_pos - HALF_PAD_HEIGHT) + paddle1_vel > HEIGHT - PAD_HEIGHT:
+        paddle1_pos -= paddle1_vel
+    if paddle2_pos + paddle2_vel > 0:
+        paddle2_pos += paddle2_vel
+    if paddle2_pos + paddle2_vel > HEIGHT - PAD_HEIGHT:
+        paddle2_pos -= paddle2_vel
 
-    
     # draw paddles
+    canvas.draw_line((0, paddle1_pos + HALF_PAD_HEIGHT), (0, paddle1_pos - HALF_PAD_HEIGHT), PAD_WIDTH, 'WHITE') #paddle1
+    canvas.draw_line((WIDTH, paddle2_pos + HALF_PAD_HEIGHT), (WIDTH, paddle2_pos - HALF_PAD_HEIGHT), PAD_WIDTH, 'WHITE') #paddle2
 
-    canvas.draw_line((paddle1_pos[0], paddle1_pos[1]), (paddle1_pos[0], paddle1_pos[1] + PAD_HEIGHT), PAD_WIDTH, 'WHITE') #paddle1
-    canvas.draw_line((paddle2_pos[0], paddle2_pos[1]), (paddle2_pos[0], paddle2_pos[1] + PAD_HEIGHT), PAD_WIDTH, 'WHITE') #paddle2
-
-
-    # determine whether paddle and ball collide    
+    # determine whether paddle and ball collide
+    if ball_pos[0] <= PAD_WIDTH + BALL_RADIUS:
+        print "hit1"
+        if ball_pos[1] > paddle1_pos - HALF_PAD_HEIGHT and ball_pos[1] < paddle1_pos + HALF_PAD_HEIGHT:
+            print "hit2"
+            ball_vel[0] = - ball_vel[0]
+        else:
+            ball_pos = [WIDTH / 2, HEIGHT / 2]
+    if ball_pos[0] >= (WIDTH - PAD_WIDTH) - BALL_RADIUS:
+        print "hit3"
+        if ball_pos[1] > paddle2_pos - HALF_PAD_HEIGHT and ball_pos[1] < paddle2_pos + HALF_PAD_HEIGHT:
+            print "hit4"
+            ball_vel[0] = - ball_vel[0]
+        else:
+            ball_pos = [WIDTH / 2, HEIGHT / 2]
     
     # draw scores
         
 def keydown(key):
     global paddle1_vel, paddle2_vel
-   
+    if key==simplegui.KEY_MAP["w"]:
+        paddle1_vel -= 4
+    if key==simplegui.KEY_MAP["s"]:
+        paddle1_vel += 4
+    if key==simplegui.KEY_MAP["up"]:
+        paddle2_vel -= 4
+    if key==simplegui.KEY_MAP["down"]:
+        paddle2_vel += 4
+
+
 def keyup(key):
     global paddle1_vel, paddle2_vel
-    
+    if key==simplegui.KEY_MAP["w"]:
+        paddle1_vel = 0
+    if key==simplegui.KEY_MAP["s"]:
+        paddle1_vel = 0
+    if key==simplegui.KEY_MAP["up"]:
+        paddle2_vel = 0
+    if key==simplegui.KEY_MAP["down"]:
+        paddle2_vel = 0
+
 
 
 # create frame
